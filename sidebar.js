8d076891bf9694d9a30b0b2dcacbb56d3dde45b2
Yves Bonneau <yves.bonneau2004@gmail.com>
2024-12-03 17:03:46 +0000

    hotfix

--- sidebar.js
+++ sidebar.js
@@ -1,42 +1,75 @@
-fetch('../calculators.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        const sidebar = document.getElementById('sidebar');
-        const pageMap = {
-            "Dynamics": "/dynamics/dynamics.html",
-            "Statics": "/statics/statics.html",
-            "Structural": "/structural/structural.html",
-            "Fluid and Aero Dynamics": "/fluid-dynamics/fluid-dynamics.html",
-            "Vibrations": "/vibrations/vibrations.html",
-            "General Mechanics": "/general_mechanics/mechanics.html"
-        };
+const sidebarData = {
+    "Dynamics": [
+        "Manual Skidpad",
+        "Driverless Skidpad",
+        "DC Skidpad",
+        "Manual Acceleration",
+        "Driverless Acceleration",
+        "DC Acceleration"
+    ],
+    "Statics": [
+        "Non Finalist Business Plan Presentation Points"
+    ],
+    "Structural": [
+        "Rectangle x bar",
+        "Rectangle y bar",
+        "Rectangle area",
+        "Rectangle I x bar",
+        "Rectangle I y bar",
+        "Triangle y bar",
+        "Triangle area",
+        "Triangle I x bar",
+        "Triangle x bar",
+        "Circle x bar",
+        "Circle y bar",
+        "Circle area with radius",
+        "Circle I x bar",
+        "Circle I y bar"
+    ],
+    "Fluid and Aero Dynamics": [
+        "Drag and Lift Calculator",
+        "Reynolds Number Calculator"
+    ],
+    "Vibrations": [
+        "Spring rate",
+        "Natural angular frequency",
+        "Damping ratio"
+    ],
+    "General Mechanics": [
+        "Velocity Equation",
+        "Positions Equation",
+        "Velocity from Positions Equation"
+    ]
+};
 
-        for (const category in data) {
-            const categoryDiv = document.createElement('div');
-            categoryDiv.className = 'category';
-            const categoryTitle = document.createElement('h2');
-            categoryTitle.innerText = category;
-            categoryTitle.addEventListener('click', () => {
-                window.location.href = pageMap[category];
-            });
-            categoryDiv.appendChild(categoryTitle);
+const pageMap = {
+    "Dynamics": "/dynamics/dynamics.html",
+    "Statics": "/statics/statics.html",
+    "Structural": "/structural/structural.html",
+    "Fluid and Aero Dynamics": "/fluid-dynamics/fluid-dynamics.html",
+    "Vibrations": "/vibrations/vibrations.html",
+    "General Mechanics": "/general_mechanics/mechanics.html"
+};
 
-            const calculatorsList = document.createElement('ul');
-            data[category].forEach(calculator => {
-                const listItem = document.createElement('li');
-                listItem.innerText = calculator;
-                calculatorsList.appendChild(listItem);
-            });
+const sidebar = document.getElementById('sidebar');
 
-            categoryDiv.appendChild(calculatorsList);
-            sidebar.appendChild(categoryDiv);
-        }
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
+for (const category in sidebarData) {
+    const categoryDiv = document.createElement('div');
+    categoryDiv.className = 'category';
+    const categoryTitle = document.createElement('h2');
+    categoryTitle.innerText = category;
+    categoryTitle.addEventListener('click', () => {
+        window.location.href = pageMap[category];
     });
+    categoryDiv.appendChild(categoryTitle);
+
+    const calculatorsList = document.createElement('ul');
+    sidebarData[category].forEach(calculator => {
+        const listItem = document.createElement('li');
+        listItem.innerText = calculator;
+        calculatorsList.appendChild(listItem);
+    });
+
+    categoryDiv.appendChild(calculatorsList);
+    sidebar.appendChild(categoryDiv);
+}
